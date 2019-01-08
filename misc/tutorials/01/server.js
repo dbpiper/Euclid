b40cb6b40fdf007835f2e6a1b844d2a19d9f9986
David Piper <david.piper@mailfence.com>
2019-01-08 17:30:33 -0600

    Starting to figure out how PostgreSQL will be used from Node.js.
    Also starting to get tooling working such as: Babel and ESLint.

--- misc/tutorials/01/server.js
+++ misc/tutorials/01/server.js
@@ -1,5 +1,24 @@
-const express = require('express');
+import express from 'express';
+import pg from 'pg';
+import Secret from './dbpiper-secret';
+
+const connectionString = process.env.DATABASE_URL
+                          || `postgres://${Secret.username}:${Secret.password}@localhost:5432/david-db`;
 const app = express();
+
+const client = new pg.Client(connectionString);
+
+(async () => {
+  await client.connect();
+  // await client.query(
+  //   `
+  //     CREATE TABLE items(
+  //       id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN
+  //     )
+  //   `,
+  // );
+  await client.end();
+})();
 
 app.get('/', (req, res) => {
   res.send('hello world');
@@ -7,7 +26,7 @@ app.get('/', (req, res) => {
 
 const server = app.listen(8081, () => {
   const host = server.address().address;
-  const port = server.address().port;
+  const { port } = server.address().port;
 
   console.log('example app listening at http://%s:%s', host, port);
 });

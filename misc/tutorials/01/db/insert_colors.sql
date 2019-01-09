21615c71c4209573155eb9ac76f707d7340d4f0a
David Piper <david.piper@mailfence.com>
2019-01-09 12:35:30 -0600

    Started access PostgreSQL using Massive.js. Testing to make sure that git-crypt works.

---
+++ misc/tutorials/01/db/insert_colors.sql
@@ -0,0 +1,13 @@
+INSERT INTO t1 (bcolor, fcolor)
+VALUES ('red', 'red'),
+       ('red', 'red'),
+       ('red', NULL),
+       (NULL, 'red'),
+       ('red', 'green'),
+       ('red', 'blue'),
+       ('green', 'red'),
+       ('green', 'blue'),
+       ('green', 'green'),
+       ('blue', 'red'),
+       ('blue', 'green'),
+       ('blue', 'blue');

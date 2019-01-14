3e415e0c0bb594c3d517713f69d2c7d611774d7a
David Piper <david.piper@mailfence.com>
2019-01-14 14:56:46 -0600

    feat: add ESLint support for both client and server
    Made some changes to the client code in order to match
    the Airbnb style guide as much as possible. However,
    I am going to *not* follow Airbnb's style guide for
    only having JSX in .jsx files as this seems silly.
    Their argument is that it is not standard JavaScript
    and thus doesn't belong in .js files. However, I personally
    don't see this as a problem (given that we have all sorts
    of non-standardized JS in our code thanks to Babel...).

--- src/client/src/screens/App/index.js
+++ src/client/src/screens/App/index.js
@@ -9,9 +9,7 @@ const App = function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit
-          {' '}
           <code>src/App.js</code>
-          {' '}
           and save to reload.
         </p>
         <a

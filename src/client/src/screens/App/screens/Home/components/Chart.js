827147921f5dd1e880caf76746bf2ce5719d6286
David Piper <david.piper@mailfence.com>
2019-01-17 16:25:09 -0600

    feat: draw stock data and style chart
    Now drawing real stock data! This is much better than trying to
    generate data that is similar to stock data. So we are pulling
    real stock data from IEX using iex-api. This allows us to work
    with data that will look and feel like what our eventual real
    data from Amazon, et al. will. In other words, we are now
    pulling real info from an API and using real dates and prices.
    
    This means that the structure of our in-progress application
    will be able to remain essentially unchanged, since the
    underlying data and asynchronous access will be very similar.
    We still need to work on formatting the x-axis as it is not
    very well laid out. In addition, the tooltip should display
    something that is more human readable than the raw day number
    like "126"!

--- src/client/src/screens/App/screens/Home/components/Chart.js
+++ src/client/src/screens/App/screens/Home/components/Chart.js
@@ -10,9 +10,12 @@ import {
   Legend,
   Line,
 } from 'recharts';
-import faker from 'faker';
+// import faker from 'faker';
 import * as R from 'ramda';
 import moment from 'moment';
+import { IEXClient } from 'iex-api';
+import * as _fetch from 'isomorphic-fetch';
+import _ from 'lodash';
 
 const ChartSection = styled.section`
   background-color: #111111;
@@ -42,59 +45,93 @@ const darkTooltipContentStyle = {
   borderRightColor: '#3d3d3d',
 };
 
-const generateData = function generateData() {
+const generateData = async function generateData() {
   const fakeArray = [];
-  const pvStart = faker.random.number();
-  const uvStart = faker.random.number();
+  // const pvStart = faker.random.number();
+  // const uvStart = faker.random.number();
+  // let pv = pvStart;
+  // let uv = uvStart;
 
-  R.range(1, 365 * 2).forEach((num) => {
+  const dateFormat = 'YYYY-MM-DD';
+  const iex = new IEXClient(_fetch);
+  const chartApple = await iex.stockChart('AAPL', '2y');
+  const chartDoj = await iex.stockChart('DIA', '2y');
+  const firstDate = moment(_.first(chartApple).date, dateFormat);
+  const joinedChart = R.zip(chartApple, chartDoj);
+  R.forEach((elem) => {
     fakeArray.push({
-      name: num,
-      pv: pvStart * (2 + num),
-      uv: uvStart * (2 * num),
+      name: moment(_.first(elem).date, dateFormat).diff(firstDate, 'days'),
+      pv: _.first(elem).high,
+      uv: _.last(elem).high,
     });
-  });
+  }, joinedChart);
+  console.log('apple');
+  console.log(chartApple);
+  console.log('doj');
+  console.log(chartDoj);
+  console.log('zipped');
+  console.log(joinedChart);
+  console.log('final');
   console.log(fakeArray);
   return fakeArray;
 };
 
-const chartData = generateData();
+class Chart extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      chartData: [],
+    };
+  }
 
-const Chart = function Chart() {
-  return (
-    <ChartSection>
-      <ChartBody>
-        <LineChart
-          width={730}
-          height={250}
-          data={chartData}
-          margin={{
-            top: 5, right: 30, left: 20, bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis
-            dataKey="name"
-            scale="time"
-            type="number"
-            domain={['dataMin', 'dataMax']}
-            interval={90}
-            // minTickGap={30}
-            // maxTickGap={30}
-            tickFormatter={tickNum => moment((Math.round(tickNum / 30) % 12) + 1, 'MM').format('MMMM')}
-            // tickFormatter={tickNum => Math.round(tickNum / 30)}
-          />
-          <YAxis />
-          <Tooltip
-            contentStyle={darkTooltipContentStyle}
-          />
-          <Legend />
-          <Line dot={false} type="monotone" dataKey="pv" stroke="#8884d8" />
-          <Line dot={false} type="monotone" dataKey="uv" stroke="#82ca9d" />
-        </LineChart>
-      </ChartBody>
-    </ChartSection>
-  );
-};
+  async componentDidMount() {
+    const chartData = await generateData();
+    this.setState({ chartData });
+  }
+
+  render() {
+    const { chartData } = this.state;
+    return (
+      <ChartSection>
+        <ChartBody>
+          <LineChart
+            width={730}
+            height={250}
+            data={chartData}
+            margin={{
+              top: 5, right: 30, left: 20, bottom: 5,
+            }}
+          >
+            <CartesianGrid
+              stroke="#3d3d3d"
+              strokeDasharray="3 3"
+            />
+            <XAxis
+              dataKey="name"
+              scale="time"
+              type="number"
+              domain={['dataMin', 'dataMax']}
+              interval={90}
+              tick={{ stroke: 'none', fill: '#c0bebb' }}
+              // minTickGap={30}
+              // maxTickGap={30}
+              tickFormatter={tickNum => moment((Math.round(tickNum / 30) % 12) + 1, 'MM').format('MMMM')}
+              // tickFormatter={tickNum => Math.round(tickNum / 30)}
+            />
+            <YAxis
+              tick={{ stroke: 'none', fill: '#c0bebb' }}
+            />
+            <Tooltip
+              contentStyle={darkTooltipContentStyle}
+            />
+            <Legend />
+            <Line dot={false} type="monotone" dataKey="pv" stroke="#8884d8" />
+            <Line dot={false} type="monotone" dataKey="uv" stroke="#82ca9d" />
+          </LineChart>
+        </ChartBody>
+      </ChartSection>
+    );
+  }
+}
 
 export default Chart;

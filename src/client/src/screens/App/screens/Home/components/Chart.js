6a3b013c4e307f25fa2e871cd95812212cb3b899
David Piper <david.piper@mailfence.com>
2019-01-17 12:25:29 -0600

    feat: add working chart with mock data
    Changes the chart component to actually display a chart! This includes
    using: Recharts.js to draw the chart, Faker.js to generate random numbers,
    and Moment.js to convert the month numbers to month names. This is doing
    essentially exactly what we want. So now we just need to add some real
    mock price data for some product X in and start working on drawing
    that data.

--- src/client/src/screens/App/screens/Home/components/Chart.js
+++ src/client/src/screens/App/screens/Home/components/Chart.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
 import styled from 'styled-components';
-
+import {
+  LineChart,
+  CartesianGrid,
+  XAxis,
+  Tooltip,
+  YAxis,
+  Legend,
+  Line,
+} from 'recharts';
+import faker from 'faker';
+import * as R from 'ramda';
+import moment from 'moment';
 
 const ChartSection = styled.section`
-  text-align: center;
   background-color: #111111;
   width: 60%;
   height: 100%;
@@ -12,20 +22,76 @@ const ChartSection = styled.section`
 `;
 
 const ChartBody = styled.div`
+  display: block;
   margin: 0;
-  margin-right: -50%;
   position: absolute;
   top: 50%;
   left: 50%;
+  left: 33%;
   transform: translate(0, -50%);
-  font-size: 2rem;
+  margin-right: -50%;
 `;
+
+
+const darkTooltipContentStyle = {
+  backgroundColor: '#18181a',
+  opacity: 1,
+  borderBottomColor: '#3d3d3d',
+  borderTopColor: '#3d3d3d',
+  borderLeftColor: '#3d3d3d',
+  borderRightColor: '#3d3d3d',
+};
+
+const generateData = function generateData() {
+  const fakeArray = [];
+  const pvStart = faker.random.number();
+  const uvStart = faker.random.number();
+
+  R.range(1, 365 * 2).forEach((num) => {
+    fakeArray.push({
+      name: num,
+      pv: pvStart * (2 + num),
+      uv: uvStart * (2 * num),
+    });
+  });
+  console.log(fakeArray);
+  return fakeArray;
+};
+
+const chartData = generateData();
 
 const Chart = function Chart() {
   return (
     <ChartSection>
       <ChartBody>
-        Chart
+        <LineChart
+          width={730}
+          height={250}
+          data={chartData}
+          margin={{
+            top: 5, right: 30, left: 20, bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis
+            dataKey="name"
+            scale="time"
+            type="number"
+            domain={['dataMin', 'dataMax']}
+            interval={90}
+            // minTickGap={30}
+            // maxTickGap={30}
+            tickFormatter={tickNum => moment((Math.round(tickNum / 30) % 12) + 1, 'MM').format('MMMM')}
+            // tickFormatter={tickNum => Math.round(tickNum / 30)}
+          />
+          <YAxis />
+          <Tooltip
+            contentStyle={darkTooltipContentStyle}
+          />
+          <Legend />
+          <Line dot={false} type="monotone" dataKey="pv" stroke="#8884d8" />
+          <Line dot={false} type="monotone" dataKey="uv" stroke="#82ca9d" />
+        </LineChart>
       </ChartBody>
     </ChartSection>
   );

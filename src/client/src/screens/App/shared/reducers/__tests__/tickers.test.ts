bbd7f3ad944fcd273dd9203896c16f29da3aedda
David Piper <david.piper@mailfence.com>
2019-02-11 11:02:38 -0600

    test: add tickers reducer test
    Add first client-side unit test, besides the existing snapshot
    tests: this tests the tickers reducer. In addition, some small
    refactoring was done to move the jest configuration out of
    the client's `package.json` and into its own `jest.config.js`
    file. This was done both to mach the server's jest configuration
    and also just to keep the client's `package.json` file more clean.
    
    Rename the testing script to make it more obvious what is
    happening, so that `test` just tests, and `test:watch` watches.
    Make the root of the client `./src` instead of `./` aka client.
    This makes references inside the project more clean, and just
    more logical in general, since the client code really shouldn't
    be accessing stuff outside of `src`.

---
+++ src/client/src/screens/App/shared/reducers/__tests__/tickers.test.ts
@@ -0,0 +1,36 @@
+import { SELECT_TICKER } from 'App/actionTypes';
+import { tickers } from '../tickers';
+
+describe('tickers reducer', () => {
+  test('should handle SELECT_TICKER action', () => {
+    expect(
+      tickers(
+        {
+          ticker: '',
+        },
+        {
+          type: SELECT_TICKER,
+          ticker: 'AAPL',
+        },
+      ),
+    ).toEqual({
+      ticker: 'AAPL',
+    });
+  });
+
+  test('should do nothing with an invalid action', () => {
+    expect(
+      tickers(
+        {
+          ticker: '',
+        },
+        {
+          type: 'the quick brown fox jumps over the lazy dog',
+          ticker: 'abc123',
+        },
+      ),
+    ).toEqual({
+      ticker: '',
+    });
+  });
+});

b04d49b5511355c5a518c6f01ab1708d778c7528
David Piper <david.piper@mailfence.com>
2019-04-18 16:27:54 -0500

    style(server): add TS definitions for the crawler module
    The crawler module is written in vanilla JS, so I have written
    my own. This should be moved into DefinitelyTyped so that
    everyone in the community can benefit from these definitions
    and not just me, as I don't want to be selfish and a code-hog.
    
    To facilitate this an issue should be made in the DefinitelyTyped
    Epic (https://github.com/dbpiper/Euclid/issues/64).
    
    Signed-off-by: David Piper <david.piper@mailfence.com>

---
+++ src/server/types/crawler/index.d.ts
@@ -0,0 +1,93 @@
+import crawler from 'crawler';
+
+import Cheerio from 'cheerio';
+import { IncomingMessage } from 'http';
+import { JSDOM } from 'jsdom';
+import Request, { Options } from 'request';
+
+declare module 'crawler' {
+  export type Encoding = string | null;
+
+  export type HttpRequestMethods =
+    | 'GET'
+    | 'HEAD'
+    | 'POST'
+    | 'PUT'
+    | 'DELETE'
+    | 'CONNECT'
+    | 'OPTIONS'
+    | 'TRACE'
+    | 'PATCH';
+
+  export interface Response extends IncomingMessage {
+    statusCode: number;
+    $: Cheerio | JSDOM;
+    charset: Encoding;
+    body: string | Buffer;
+    headers: object;
+    request: Request;
+    options: CrawlerOptions;
+  }
+
+  export type CallbackFunction = (
+    error: Error | string | undefined,
+    res: Response,
+    done: () => void,
+  ) => void;
+
+  export interface CrawlerOptions extends Options {
+    // Basic request options
+    uri: string;
+    timeout: number;
+    callback: CallbackFunction;
+
+    // Schedule options
+    maxConnections: number;
+    rateLimit: number;
+    priorityRange: number;
+    priority: number;
+
+    // Retry options
+    retries: number;
+    retryTimeout: number;
+
+    // Server-side DOM options
+    jQuery: boolean | string | object;
+
+    // Charset encoding
+    forceUTF8: boolean;
+    incomingEncoding: Encoding;
+
+    // Cache
+    skipDuplicates: boolean;
+
+    // Http headers
+    rotateUA: boolean;
+    userAgent: string | [];
+    referer: boolean;
+    headers: object;
+
+    // Other options
+    skipEventRequest: boolean;
+    autoWindowClose: boolean;
+    gzip: boolean;
+    method: HttpRequestMethods;
+    homogeneous: boolean;
+  }
+
+  export type OnEventFunction = (options: CrawlerOptions) => void;
+
+  declare class Crawler {
+    constructor(options: CrawlerOptions);
+    public queue(options: CrawlerOptions): void;
+    public setLimiterProperty(
+      limiter: string,
+      property: string,
+      value: number,
+    ): void;
+    public on(event: string, callback: OnEventFunction): void;
+    public queueSize(): number;
+  }
+
+  export default Crawler;
+}
